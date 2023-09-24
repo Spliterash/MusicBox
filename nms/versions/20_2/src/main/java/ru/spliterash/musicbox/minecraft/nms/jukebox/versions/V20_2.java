33c5d9c43a941969120bf421a23e0e1cc61aa300
VitSun666 <61446535+VitSun666@users.noreply.github.com>
2023-09-24 22:48:46 +0300

    1.20.2

---
+++ nms/versions/20_2/src/main/java/ru/spliterash/musicbox/minecraft/nms/jukebox/versions/V20_2.java
@@ -0,0 +1,31 @@
+package ru.spliterash.musicbox.minecraft.nms.jukebox.versions;
+
+import net.minecraft.world.level.block.entity.JukeboxBlockEntity;
+import org.bukkit.block.Jukebox;
+import org.bukkit.craftbukkit.v1_20_R2.block.CraftJukebox;
+import org.bukkit.craftbukkit.v1_20_R2.inventory.CraftItemStack;
+import org.bukkit.inventory.ItemStack;
+import ru.spliterash.musicbox.minecraft.nms.jukebox.IJukebox;
+
+public class V20_2 implements IJukebox {
+    private final JukeboxBlockEntity tileEntity;
+
+    public V20_2(Jukebox jukebox) {
+        CraftJukebox craft = (CraftJukebox) jukebox;
+        tileEntity = craft.getTileEntity();
+    }
+
+    public void setJukebox(ItemStack item) {
+        net.minecraft.world.item.ItemStack converted = CraftItemStack.asNMSCopy(item);
+        tileEntity.setRecordWithoutPlaying(converted);
+
+    }
+
+    public ItemStack getJukebox() {
+        net.minecraft.world.item.ItemStack nmsItem = tileEntity.getItem(0);
+        if (nmsItem.isEmpty())
+            return null;
+
+        return CraftItemStack.asBukkitCopy(nmsItem);
+    }
+}
--- plugin/build.gradle.kts
+++ plugin/build.gradle.kts
@@ -7,8 +7,8 @@ dependencies {
     compileOnly("com.github.MilkBowl:VaultAPI:1.7")
     compileOnly("com.github.koca2000:NoteBlockAPI:1.6.1")
 
-    api("com.github.cryptomorin:XSeries:9.4.0")
-    api("io.github.bananapuncher714:nbteditor:7.18.6")
+    api("com.github.cryptomorin:XSeries:9.5.0")
+    api("io.github.bananapuncher714:nbteditor:7.18.7")
     api("org.bstats:bstats-bukkit:3.0.2")
 
     api(project(":nms"))
